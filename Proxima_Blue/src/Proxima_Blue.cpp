9cfa5ef3caa44bdd1100cf8dc83bb48242318fb2
Nicole Bouligny <liv.bouligny@gmail.com>
2022-08-17 12:57:29 -0600

    Proxima Blue 0.1 - median function

--- Proxima_Blue/src/Proxima_Blue.cpp
+++ Proxima_Blue/src/Proxima_Blue.cpp
@@ -15,8 +15,8 @@
 
 void setup();
 void loop();
-void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context);
-void getArgonSignalData();
+int isRecognized(byte addr1, byte addr0);
+int getMedian(int _rssiArray[], int _count);
 #line 10 "c:/Users/Jason.000/Documents/IoT/Proxima_Blue/Proxima_Blue/src/Proxima_Blue.ino"
 SYSTEM_THREAD(ENABLED);
 
@@ -35,13 +35,17 @@ const BleUuid serviceUuid("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
 const BleUuid rxUuid("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
 const BleUuid txUuid("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
 
-unsigned long lastScan;
+unsigned long lastScan, last;
 BleAddress peripheralAddr;
-int rssi;
+int rssi, i, j, neoSig, infiniSig, roombaSig, scanCount, duckCount, infiniCount, roombaCount;
+int neoDuck[50];
+int infinity[50];
+int roomba[50];
 byte scanMAC[50];
 
+
 BleCharacteristic txCharacteristic("tx", BleCharacteristicProperty::NOTIFY, txUuid, serviceUuid);
-BleCharacteristic rxCharacteristic("rx", BleCharacteristicProperty::WRITE_WO_RSP, rxUuid, serviceUuid, onDataReceived, NULL);
+// BleCharacteristic rxCharacteristic("rx", BleCharacteristicProperty::WRITE_WO_RSP, rxUuid, serviceUuid, onDataReceived, NULL);
 BleAdvertisingData data;
 
 BleScanResult scanResults[SCAN_RESULT_MAX];
@@ -59,7 +63,7 @@ void setup() {
 
   BLE.on ();
   BLE.addCharacteristic(txCharacteristic);
-  BLE.addCharacteristic(rxCharacteristic);
+  // BLE.addCharacteristic(rxCharacteristic);
   data.appendServiceUUID(serviceUuid);
   BLE.advertise(&data);
   BLE.setTxPower(8);
@@ -75,145 +79,144 @@ void setup() {
   display.setTextColor(WHITE);
   display.setCursor(0,0);
   display.display();
-
 }
 
 void loop() {
-  //Vector Scan
-  Vector<BleScanResult> scanResults = BLE.scan();
-
-    if (scanResults.size()) {
-      Log.info("%d devices found", scanResults.size());
-
-      for (int ii = 0; ii < scanResults.size(); ii++) {
-        // For Device OS 2.x and earlier, use scanResults[ii].address[0], etc. without the ()
-        Log.info("MAC: %02X:%02X:%02X:%02X:%02X:%02X | RSSI: %i dBm",
-          scanResults[ii].address()[5], scanResults[ii].address()[4], scanResults[ii].address()[3],
-          scanResults[ii].address()[2], scanResults[ii].address()[1], scanResults[ii].address()[0], scanResults[ii].rssi());
-        sprintf((char *)scanMAC,"MAC: %02X:%02X:%02X:%02X:%02X:%02X\nRSSI: %i dBm\n",scanResults[ii].address()[5], scanResults[ii].address()[4], 
-          scanResults[ii].address()[3], scanResults[ii].address()[2], scanResults[ii].address()[1], scanResults[ii].address()[0], scanResults[ii].rssi());
-        // if (scanResults[ii].rssi() > -85) {
-          txCharacteristic.setValue(scanMAC, 50);
-        // }
-        String name = scanResults[ii].advertisingData().deviceName();
-        if (name.length() > 0) {
-          Log.info("Advertising name: %s", name.c_str());
+  if (millis() - last > 60000) {
+    scanCount = 0;
+    for (j = 0; j < 10; j++) {
+      scanCount++;
+      Serial.printf("Scan %i\n",scanCount);      
+        //Vector Scan
+      Vector<BleScanResult> scanResults = BLE.scan();
+      if (scanResults.size()) {
+        Log.info("%d devices found", scanResults.size());
+        for (i = 0; i < scanResults.size(); i++) {
+          // For Device OS 2.x and earlier, use scanResults[i].address[0], etc. without the ()
+          // Log.info("MAC: %02X:%02X:%02X:%02X:%02X:%02X | RSSI: %i dBm",
+          //   scanResults[i].address()[5], scanResults[i].address()[4], scanResults[i].address()[3],
+          //   scanResults[i].address()[2], scanResults[i].address()[1], scanResults[i].address()[0], scanResults[i].rssi());
+          sprintf((char *)scanMAC,"MAC: %02X:%02X:%02X:%02X:%02X:%02X\nRSSI: %i dBm\n",scanResults[i].address()[5], scanResults[i].address()[4], 
+            scanResults[i].address()[3], scanResults[i].address()[2], scanResults[i].address()[1], scanResults[i].address()[0], scanResults[i].rssi());
+          if (isRecognized(scanResults[i].address()[1], scanResults[i].address()[0]) == 1) {            
+            neoDuck[duckCount] = scanResults[i].rssi();
+            txCharacteristic.setValue(scanMAC, 50);
+          }  
+          if (isRecognized(scanResults[i].address()[1], scanResults[i].address()[0]) == 2) { 
+              infinity[infiniCount] = scanResults[i].rssi();
+              txCharacteristic.setValue(scanMAC, 50);
+          }
+          if (isRecognized(scanResults[i].address()[1], scanResults[i].address()[0]) == 3) { 
+              roomba[roombaCount] = scanResults[i].rssi();
+              txCharacteristic.setValue(scanMAC, 50);
+          }           
+          // String name = scanResults[i].advertisingData().deviceName();
+          // if (name.length() > 0) {
+          //   Log.info("Advertising name: %s", name.c_str());
+          // }
         }
       }
     }
-    delay(3000);
-
-  // if (millis() - lastScan > 500) {
-  //   lastScan = millis();
-
-  //   rssi = 0;
-  //   BLE.scan(scanResultCallback, NULL);
-
-  //   // display.clearDisplay();
-  //   if (rssi) {
-  //     display.setTextSize(2);
-  //     display.setTextColor(WHITE);
-  //     display.setCursor(0,0);
-
-  //     char buf[32];
-  //     snprintf(buf, sizeof(buf), "%i\n",rssi);
-  //     display.println(buf);
-  //   }
-  //   display.display();
-  // }
-
-  // Only scan for 500 milliseconds
-  // BLE.setScanTimeout(500);
-  // int count = BLE.scan(scanResults,SCAN_RESULT_MAX);
-
-  // uint32_t curColorCode;
-  // int curRssi = -999;
-
-  // for (int ii = 0; ii < count; ii++) {
-  //   uint8_t buf[BLE_MAX_ADV_DATA_LEN];
-  //   size_t len;
-  // When getting a specific AD Type, the length returned does not include the length or AD Type so len will be one less
-  // than what we put in the beacon code, because that includes the AD Type.
-    // len = scanResults[ii].advertisingData().get(BleAdvertisingDataType::MANUFACTURER_SPECIFIC_DATA, buf, BLE_MAX_ADV_DATA_LEN);
-    // if (len == 7) {
-      // We have manufacturer-specific advertising data (0xff) and it's 7 bytes (without the AD type)
-
-			// Byte: BLE_SIG_AD_TYPE_MANUFACTURER_SPECIFIC_DATA (0xff)
-			// 16-bit: Company ID (0xffff)
-			// Byte: Internal packet identifier (0x55)
-			// 32-bit: Color code
-    //   if (buf[0] == 0xff && buf[1] == 0xff && buf[2] == 0x55) {
-    //     uint32_t colorCode;
-    //     memcpy(&colorCode, &buf[3], 4);
-
-    //     Log.info("colorCode: 0x%lx rssi=%d address=%02X:%02X:%02X:%02X:%02X:%02X ",
-    //       colorCode, scanResults[ii].rssi(),scanResults[ii].address()[0],scanResults[ii].address()[1], scanResults[ii].address()[2],
-    //       scanResults[ii].address()[3], scanResults[ii].address()[4], scanResults[ii].address()[5]);
-    //     Serial.printf("colorCode: 0x%lx rssi=%d address=%02X:%02X:%02X:%02X:%02X:%02X ",
-    //       colorCode, scanResults[ii].rssi(),scanResults[ii].address()[0],scanResults[ii].address()[1], scanResults[ii].address()[2],
-    //       scanResults[ii].address()[3], scanResults[ii].address()[4], scanResults[ii].address()[5]); 
-    //     if (scanResults[ii].rssi() > curRssi) {
-    //       // Show whatever device has the strongest signal
-    //       curRssi = scanResults[ii].rssi();
-		// 			curColorCode = colorCode;          
-    //     }
-    //   }
-    // }
-  // }
-  // if (curRssi != -999) {
-  //   ledOverride.setColor(curColorCode);
-  //   ledOverride.setActive(true);
-  // }
-  // else {
-  //   ledOverride.setActive(false);
-  // }
+    neoSig = getMedian(neoDuck, duckCount);
+    infiniSig = getMedian(infinity, infiniCount);
+    roombaSig = getMedian(roomba, roombaCount);
+    Serial.printf("====================\nDistance\nNeoPixel: %i\nInfinity Cube: %i\nRoomba: %i\n====================\n", neoSig, infiniSig, roombaSig);
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.printf("====================\nDistance\nNeoPixel: %i\nInfinity Cube: %i\nRoomba: %i\n====================\n", neoSig, infiniSig, roombaSig);
+    display.display();
+    last = millis();
+    BLE.stopScanning();
+  }    
 }
 
-void scanResultCallback(const BleScanResult *scanResult, void *context) {
+// void scanResultCallback(const BleScanResult *scanResult, void *context) {
 
-  BleUuid foundServiceUuid;
-	size_t svcCount = scanResult->advertisingData().serviceUUID(&foundServiceUuid, 1);
-	if (svcCount == 0 || !(foundServiceUuid == serviceUuid)) {
-		/*
-		Log.info("ignoring %02X:%02X:%02X:%02X:%02X:%02X, not our service",
-				scanResult->address()[0], scanResult->address()[1], scanResult->address()[2],
-				scanResult->address()[3], scanResult->address()[4], scanResult->address()[5]);
-		*/
-		return;
-	}
-  display.clearDisplay();
-	Log.info("rssi=%d \nserver=%02X:%02X:%02X:%02X:%02X:%02X\n",
-    scanResult->rssi(), scanResult->address()[5], scanResult->address()[4], scanResult->address()[3], 
-    scanResult->address()[2], scanResult->address()[1], scanResult->address()[0]);
+//   BleUuid foundServiceUuid;
+// 	size_t svcCount = scanResult->advertisingData().serviceUUID(&foundServiceUuid, 1);
+// 	if (svcCount == 0 || !(foundServiceUuid == serviceUuid)) {
+// 		/*
+// 		Log.info("ignoring %02X:%02X:%02X:%02X:%02X:%02X, not our service",
+// 				scanResult->address()[0], scanResult->address()[1], scanResult->address()[2],
+// 				scanResult->address()[3], scanResult->address()[4], scanResult->address()[5]);
+// 		*/
+// 		return;
+// 	}
+//   display.clearDisplay();
+// 	Log.info("rssi=%d \nserver=%02X:%02X:%02X:%02X:%02X:%02X\n",
+//     scanResult->rssi(), scanResult->address()[5], scanResult->address()[4], scanResult->address()[3], 
+//     scanResult->address()[2], scanResult->address()[1], scanResult->address()[0]);
 
-  Serial.printf("rssi=%d\nserver= %02X:%02X:%02X:%02X:%02X:%02X\n",
-    scanResult->rssi(), scanResult->address()[5], scanResult->address()[4], scanResult->address()[3], 
-    scanResult->address()[2], scanResult->address()[1], scanResult->address()[0]);
+//   Serial.printf("rssi=%d\nserver= %02X:%02X:%02X:%02X:%02X:%02X\n",
+//     scanResult->rssi(), scanResult->address()[5], scanResult->address()[4], scanResult->address()[3], 
+//     scanResult->address()[2], scanResult->address()[1], scanResult->address()[0]);
 
-  display.setTextSize(1);
-  display.printf("rssi=%d\nserver= \n%02X:%02X:%02X:%02X:%02X:%02X\n",
-    scanResult->rssi(), scanResult->address()[5], scanResult->address()[4], scanResult->address()[3], 
-    scanResult->address()[2], scanResult->address()[1], scanResult->address()[0]);
-  display.display();
-	peripheralAddr = scanResult->address();
-	rssi = scanResult->rssi();
+//   display.setTextSize(1);
+//   display.printf("rssi=%d\nserver= \n%02X:%02X:%02X:%02X:%02X:%02X\n",
+//     scanResult->rssi(), scanResult->address()[5], scanResult->address()[4], scanResult->address()[3], 
+//     scanResult->address()[2], scanResult->address()[1], scanResult->address()[0]);
+//   display.display();
+// 	peripheralAddr = scanResult->address();
+// 	rssi = scanResult->rssi();
 	
-	BLE.stopScanning();
-}
+// 	BLE.stopScanning();
+// }
 
-void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context) {
-  uint8_t i;
+// void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context) {
+//   uint8_t i;
   
-  Serial.printf("Received data from:%02X:%02X:%02X:%02X:%02X:%02X\n",peer.address()[0], peer.address()[1], peer.address()[2], peer.address()[3], peer.address()[4], peer.address()[5]);
-  Serial.printf("Bytes: ");
+//   Serial.printf("Received data from:%02X:%02X:%02X:%02X:%02X:%02X\n",peer.address()[0], peer.address()[1], peer.address()[2], peer.address()[3], peer.address()[4], peer.address()[5]);
+//   Serial.printf("Bytes: ");
 
-  for (i = 0;i < len; i++) {
-    Serial.printf("%02X ",data[i]);
+//   for (i = 0;i < len; i++) {
+//     Serial.printf("%02X ",data[i]);
+//   }
+//   Serial.printf("\n");
+//   Serial.printf("Message: %s\n",(char *)data);
+// }
+
+int isRecognized(byte addr1, byte addr0) {
+  if (addr1 == (0xF0 || 0xEF || 0xC0)) {
+    switch (addr1) { //identify if device is recognized and return value correspondent to device
+      case 0xA8:        
+        return 2;
+        break;        
+      case 0xEF:        
+        return 3;
+        break;        
+      case 0xC0:
+        return 1;
+        break;
+      default:
+        return 0;
+        break;
+    }
   }
-  Serial.printf("\n");
-  Serial.printf("Message: %s\n",(char *)data);
+  else {
+    return 0;
+  }
 }
 
-void getArgonSignalData() {
-
+int getMedian(int _rssiArray[], int _count) {
+  int medArray[_count];
+  for (byte jj = 0; jj < _count; jj++) {
+    medArray[jj] = _rssiArray[jj];
+  }
+  int ii, jj, temp;
+  for (jj = 0; jj < _count - 1; jj++) {
+    for (ii = 0; ii < _count - jj - 1; ii++) {
+      if (medArray[ii] > medArray[ii+1]) {      
+        temp = medArray[ii];
+        medArray[ii] = medArray[ii+1];
+        medArray[ii+1] = temp;
+      }
+    }
+  }  
+  if ((_count & 1) > 0) {
+    temp = medArray[(_count - 1) / 2];
+  }
+  else {
+    temp = (medArray[_count / 2] + medArray[(_count / 2)-1]) / 2;
+  }
+  return temp;
 }
